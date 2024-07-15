49715cb091d9b8985384584d0967d6574a563f8f
dev4pear <tommycole519@gmail.com>
2024-07-15 19:53:52 -0400

    update app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const onVerify = () => {
+    // window.close();
+    window.Telegram.WebApp.close();
+  };
   return (
-    <div className="App">
-      <div className="text-5xl">aaa</div>
+    <div className="App flex flex-col gap-4 self-center items-center">
+      <div className="pt-4 text-3xl font-bold">Human Verification</div>
+      <div className="font-bold">Verify below to be granted entry</div>
+      <div
+        onClick={() => onVerify()}
+        className="w-[240px] bg-green-700 text-white py-3 rounded-md"
+      >
+        Click here
+      </div>
     </div>
   );
 }
